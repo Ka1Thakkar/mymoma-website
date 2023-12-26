7596c25c560c07a8a9bb4f7330d878ef1aa58d2b
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2023-12-26 17:05:24 +0530

    Mentor text issue solved

--- app/AboutUs/components/OurMentor.jsx
+++ app/AboutUs/components/OurMentor.jsx
@@ -15,7 +15,7 @@ const OurMentors = () => {
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 px-10 pt-20 gap-x-5 gap-y-5'>
                 {mentors.map((data, i)=>{
                     return (
-                        <div key={i} className=' group relative'>
+                        <div key={i} className=' group relative overflow-hidden'>
                             <div className='bg-mentorpink/80 text-black absolute h-0 w-[100%] group-hover:opacity-100 opacity-0 transition-all ease-in-out flex flex-col items-center justify-center p-2 group-hover:h-full duration-500 bottom-0 delay-75 gap-2'>
                                 <h1 className='lg:text-[25px] text-[24px] font-semibold text-center'>{data.name}</h1>
                                 <h1 className='lg:text-[23px] text-[22px] text-center'>{data.position}</h1>
